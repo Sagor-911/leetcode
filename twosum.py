66d2637a3e994975dbce65c2eef2cbcc1f941573
AL-AMIN <alamin.sagor@g.bracu.ac.bd>
2026-02-20 19:32:03 +0600

    1. Two Sum problem solved - Easy

---
+++ twosum.py
@@ -0,0 +1,16 @@
+#1. Two Sum - Easy
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return str([i, j])
+    
+sum = Solution()
+print(sum.twoSum([3,3], 6))
